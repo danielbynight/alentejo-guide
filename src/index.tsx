ab80256ca0f506ca447da9a56bd24a02c8e4f3bf
Daniel Matias Ferrer <xassbit@gmail.com>
2020-06-16 22:35:20 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './styles/reset.scss'
+
+const App = () => <div>Hello world</div>
+
+ReactDOM.render(
+        <App />,
+    document.getElementById('root')
+)
